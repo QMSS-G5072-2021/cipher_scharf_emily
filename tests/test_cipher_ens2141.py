7ac55d93fd4bb08abc2ceb9e5dc5987ae2d5d645
Emily Nina Scharf <90808227+EmilyNina@users.noreply.github.com>
2021-11-09 21:38:57 -0500

    question 3

--- tests/test_cipher_ens2141.py
+++ tests/test_cipher_ens2141.py
@@ -1,1 +1,9 @@
 from cipher_ens2141 import cipher_ens2141
+
+def test_cipher():
+    example = 'LAZY'
+    expected = 'iXwv'
+    actual = cipher(text = example, shift = 23)
+    print(actual, expected)
+    assert actual == expected
+    print ('Success')
